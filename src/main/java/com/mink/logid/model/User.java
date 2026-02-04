20ddffcb0d95d14b38b051695bb0461b55475bbc
zad <kimminkyeong0418@gmail.com>
2026-02-04 19:47:02 +0900

    feat : user model 생성

---
+++ src/main/java/com/mink/logid/model/User.java
@@ -0,0 +1,29 @@
+package com.mink.logid.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@AllArgsConstructor
+public class User extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String pw;
+
+    @Column(nullable = false)
+    private boolean enabled;
+
+
+}
